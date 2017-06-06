eac5f76eca79dcb514b94c28de6b16e4e636eb6c
Mark Underseth <marku@s2technologies.com>
2017-06-06 15:36:08 +0000

    fix typos

--- standalone-trigger.sh
+++ standalone-trigger.sh
@@ -5,4 +5,4 @@ date >> trigger-commit.txt
 git add trigger-commit.txt
 git commit -m "trigger Standalone CI builds ..."
 #git remote add upstream https://$GH_TOKEN@github.com/munderseth/ci-sandbox.git
-git push https://$GH_TOKEN@github.com/munderseth/ci-sandbox.git qstandalone
+git push https://$GH_TOKEN@github.com/munderseth/ci.sandbox.git standalone
