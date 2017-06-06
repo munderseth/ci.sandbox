5735137fd5645d693431813a7a753db10b56b4c8
Mark Underseth <marku@s2technologies.com>
2017-06-06 15:38:34 +0000

    back to master

--- standalone-trigger.sh
+++ standalone-trigger.sh
@@ -6,3 +6,4 @@ git add commit-trigger.txt
 git commit -m "trigger Standalone CI builds ..."
 #git remote add upstream https://$GH_TOKEN@github.com/munderseth/ci-sandbox.git
 git push https://$GH_TOKEN@github.com/munderseth/ci.sandbox.git standalone
+git checkout master
