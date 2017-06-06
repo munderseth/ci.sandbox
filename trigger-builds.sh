6b81d04ab86b039dd1f0f5a8be47e9c49e0ff3aa
munderseth <marku@s2technologies.com>
2017-06-06 16:42:56 +0000

    what.1

--- trigger-builds.sh
+++ trigger-builds.sh
@@ -6,13 +6,16 @@ setup_git() {
 }
 
 trigger() {
-  git pull https://$GH_TOKEN@github.com/munderseth/ci.sandbox.git
+#  git pull https://$GH_TOKEN@github.com/munderseth/ci.sandbox.git
+  git clone https://$GH_TOKEN@github.com/munderseth/ci.sandbox.git
+  cd ci.sandbox
   git checkout $1
   date >> commit-trigger.txt
   git add commit-trigger.txt
   git commit -m "trigger $1 CI build(s) ..."
   git push https://$GH_TOKEN@github.com/munderseth/ci.sandbox.git $1
-  git checkout master
+  cd ..
+#  git checkout master
 }
 
 setup_git
