62d4609934003721c88cc202c203c30eb62dfec7
Mark Underseth <marku@s2technologies.com>
2017-06-06 15:37:32 +0000

    more typos

--- standalone-trigger.sh
+++ standalone-trigger.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 git checkout standalone
-date >> trigger-commit.txt
-git add trigger-commit.txt
+date >> commit-trigger.txt
+git add commit-trigger.txt
 git commit -m "trigger Standalone CI builds ..."
 #git remote add upstream https://$GH_TOKEN@github.com/munderseth/ci-sandbox.git
 git push https://$GH_TOKEN@github.com/munderseth/ci.sandbox.git standalone
